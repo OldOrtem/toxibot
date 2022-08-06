f835c1ca4f359f94302fd93ca756a7a035464cc2
OldOrtem <artemsmirnof@mail.ru>
2022-08-06 17:14:08 +0300

    Create keyboardmenu.py

---
+++ src/keyboards/text/keyboardmenu.py
@@ -0,0 +1,37 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Текстом"),
+        ],
+        [
+            KeyboardButton(text="Голосом"),
+        ],
+    ],
+    resize_keyboard=True
+)
+
+test_q1 = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="гей"),
+        ],
+        [
+            KeyboardButton(text="лесбуха"),
+        ],
+    ],
+    resize_keyboard=True
+)
+
+test_q2 = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="педофил"),
+        ],
+        [
+            KeyboardButton(text="пидорас"),
+        ],
+    ],
+    resize_keyboard=True
+)
