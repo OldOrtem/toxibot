4195512e42dcc281b05969092037f74b9a539a8e
OldOrtem <artemsmirnof@mail.ru>
2022-08-06 17:14:15 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
-from calc import c
-print("Hello world")
-print(c)
-s= input()
+import requests
+import aiogram
+import asyncio
+
+from config import BOT_TOKEN
+
+
+
+if __name__ == "__main__":
+    from aiogram import executor
+    from handlers import dp
+    from handlers import send_to_admin
+    executor.start_polling(dp, on_startup=send_to_admin)
